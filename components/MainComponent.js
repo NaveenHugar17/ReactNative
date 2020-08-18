0022da0aff02c108e3e501bfe9fd985babceb1eb
Naveen Hugar <naveenhugarnh21@gmail.com>
2020-08-18 09:10:44 +0530

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Constants from 'expo-constants';
 import Dishdetail from './DishdetailComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
@@ -84,6 +85,23 @@ const ContactNavigator = createStackNavigator({
         onPress={ () => navigation.toggleDrawer() } /> 
   })
 });
+
+const ReservationNavigator = createStackNavigator({
+  Reservation: { screen: Reservation }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.navigate('DrawerToggle') } />    
+  })
+})
 
 const AboutNavigator = createStackNavigator({
   About: { screen: About }
@@ -181,6 +199,21 @@ const MainNavigator = createDrawerNavigator({
               />
         )
       },
+    },
+  Reservation:
+    { screen: ReservationNavigator,
+        navigationOptions: {
+          title: 'Reserve Table',
+          drawerLabel: 'Reserve Table',
+          drawerIcon: ({ tintColor, focused }) => (
+            <Icon
+              name='cutlery'
+              type='font-awesome'            
+              size={24}
+              iconStyle={{ color: tintColor }}
+            />
+          ),
+        }
     }
 }, {
 drawerBackgroundColor: '#D1C4E9',
