aee888736be1ac7ff7a077569b028e69af79cbaf
Naveen Hugar <naveenhugarnh21@gmail.com>
2020-08-22 11:56:26 +0530

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,6 +7,7 @@ import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-nativ
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import Favorites from './FavoriteComponent';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -88,6 +89,23 @@ const ContactNavigator = createStackNavigator({
 
 const ReservationNavigator = createStackNavigator({
   Reservation: { screen: Reservation }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff",
+    headerLeft: <Icon name="menu" size={24}
+      iconStyle={{ color: 'white' }} 
+      onPress={ () => navigation.navigate('DrawerToggle') } />    
+  })
+})
+
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
 }, {
   navigationOptions: ({ navigation }) => ({
     headerStyle: {
@@ -200,6 +218,21 @@ const MainNavigator = createDrawerNavigator({
         )
       },
     },
+  Favorites:
+    { screen: FavoritesNavigator,
+      navigationOptions: {
+        title: 'My Favorites',
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon
+            name='heart'
+            type='font-awesome'            
+            size={24}
+            iconStyle={{ color: tintColor }}
+          />
+        ),
+      }
+    }, 
   Reservation:
     { screen: ReservationNavigator,
         navigationOptions: {
