0cf3a42346df77f53663ab1a91bd8bfe71333ea0
Naveen Hugar <naveenhugarnh21@gmail.com>
2020-08-26 11:44:08 +0530

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -8,6 +8,7 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -138,6 +139,23 @@ const AboutNavigator = createStackNavigator({
   })
 });
 
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login }
+}, {
+navigationOptions: ({ navigation }) => ({
+  headerStyle: {
+      backgroundColor: "#512DA8"
+  },
+  headerTitleStyle: {
+      color: "#fff"            
+  },
+  headerTintColor: "#fff",
+  headerLeft: <Icon name="menu" size={24}
+    iconStyle={{ color: 'white' }} 
+    onPress={ () => navigation.toggleDrawer() } />    
+})
+});
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -155,6 +173,21 @@ const CustomDrawerContentComponent = (props) => (
 );
 
 const MainNavigator = createDrawerNavigator({
+  Login: 
+  { screen: LoginNavigator,
+    navigationOptions: {
+      title: 'Login',
+      drawerLabel: 'Login',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='sign-in'
+          type='font-awesome'            
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
   Home: 
     { screen: HomeNavigator,
       navigationOptions: {
@@ -249,6 +282,7 @@ const MainNavigator = createDrawerNavigator({
         }
     }
 }, {
+  initialRouteName: 'Home',
 drawerBackgroundColor: '#D1C4E9',
 contentComponent: CustomDrawerContentComponent
 });
